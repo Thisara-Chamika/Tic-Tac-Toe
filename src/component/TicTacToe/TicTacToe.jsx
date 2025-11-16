d75263ebe0fe6ce2eb03b367a516a2fe57f7bd41
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-11-16 23:37:56 +0530

    make basic structure and adding css

--- src/component/TicTacToe/TicTacToe.jsx
+++ src/component/TicTacToe/TicTacToe.jsx
@@ -5,8 +5,26 @@ import circle_icon from "../assets/circle.png"
 
 export const TicTacToe = () => {
   return (
-    <div>
-        
+    <div className='container'>
+        <h1 className="title">Tic Tac Toe Game in <span>React</span></h1>
+        <div className="board">
+            <div className="row1">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
+            <div className="row2">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
+            <div className="row3">
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+                <div className="boxes"></div>
+            </div>
+        </div>
+        <button className="reset">Reset</button>
     </div>
   )
 }
