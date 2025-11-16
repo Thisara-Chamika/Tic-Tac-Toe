fd6abe03908d31faaef478c289d2083618568075
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-11-16 22:34:18 +0530

    Initialize the project

---
+++ src/component/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TicTacToe.css'
+import cross_icon from "../assets/cross.png"
+import circle_icon from "../assets/circle.png"
+
+export const TicTacToe = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
